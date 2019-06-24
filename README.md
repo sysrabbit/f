b696a387377be6429aea7f3c14d7d28f485f8054
Michael Borders <michael.a.borders@gmail.com>
2019-06-24 18:45:17 -0400

    Parallelized program
    Now f output ocurrs on 4 threads for 4x the f throughput. That's a lot
    of respects.

--- README.md
+++ README.md
@@ -4,10 +4,15 @@
 
 If it wasn't obvious, this program was a joke I created for internet points when someone decided to do the above task in python, then someone one upped them in c, I felt my self capable of one upping them in assembly.
 
+## Features
+
+- Easy to remember syntax
+- Parallelized outputting of f for increased f throughput
+
 ## Roadmap
 
+- Dynamically detect the number of threads available to maximize f throughput
 - Explore gcc compiliation for optimized builds (I don't really know how gcc fares against nasm)
-- Parallelize outputting of f for increased f throughput for maximum paying of respects
 - Rewrite in pure binary so that no compiler is needed for even further optimization and increase in f throughput
 
 ## License
--- f.asm
+++ f.asm
@@ -1,12 +1,72 @@
+bits 64
+global _start
+
+;; sys/syscall.h
+%define SYS_write	1
+%define SYS_mmap	9
+%define SYS_clone	56
+%define SYS_exit	60
+
+;; unistd.h
+%define STDIN		0
+%define STDOUT		1
+%define STDERR		2
+
+;; sched.h
+%define CLONE_VM	0x00000100
+%define CLONE_FS	0x00000200
+%define CLONE_FILES	0x00000400
+%define CLONE_SIGHAND	0x00000800
+%define CLONE_PARENT	0x00008000
+%define CLONE_THREAD	0x00010000
+%define CLONE_IO	0x80000000
+
+;; sys/mman.h
+%define MAP_GROWSDOWN	0x0100
+%define MAP_ANONYMOUS	0x0020
+%define MAP_PRIVATE	0x0002
+%define PROT_READ	0x1
+%define PROT_WRITE	0x2
+%define PROT_EXEC	0x4
+
+%define THREAD_FLAGS \
+ CLONE_VM|CLONE_FS|CLONE_FILES|CLONE_SIGHAND|CLONE_PARENT|CLONE_THREAD|CLONE_IO
+
+%define STACK_SIZE	(4096 * 1024)
+
 section .text
-    global _start
 _start:
+	mov rdi, f
+	call thread_create
+	mov rdi, f
+	call thread_create
+    mov rdi, f
+	call thread_create
+    call f
+thread_create:
+	push rdi
+	call stack_create
+	lea rsi, [rax + STACK_SIZE - 8]
+	pop qword [rsi]
+	mov rdi, THREAD_FLAGS
+	mov rax, SYS_clone
+	syscall
+	ret
+stack_create:
+	mov rdi, 0
+	mov rsi, STACK_SIZE
+	mov rdx, PROT_WRITE | PROT_READ
+	mov r10, MAP_ANONYMOUS | MAP_PRIVATE | MAP_GROWSDOWN
+	mov rax, SYS_mmap
+	syscall
+	ret
+f:
     mov     edx,len
     mov     ecx,msg
     mov     ebx,1
     mov     eax,4
     int     0x80
-    jmp     _start
+    jmp     f
 section .data
 msg db      "f",0xa
 len equ     $ - msg
